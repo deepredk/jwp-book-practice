54171610e9d6df31c22e90011edcee0e8466bf52
deepredk <deepred.kim@gmail.com>
2022-03-15 20:54:49 +0900

    45p 계산기 테스트 클래스

---
+++ chapter01/src/next/step/calculator/CalculatorTest.java
@@ -0,0 +1,11 @@
+package next.step.calculator;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(9, 3));
+        System.out.println(cal.subtract(9, 3));
+        System.out.println(cal.multiply(9, 3));
+        System.out.println(cal.divide(9, 3));
+    }
+}
