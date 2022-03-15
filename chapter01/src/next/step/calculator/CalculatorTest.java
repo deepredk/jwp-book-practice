08eb6ccf3f058d8512e697792013f65530d444aa
deepredk <deepred.kim@gmail.com>
2022-03-15 20:57:18 +0900

    46p 계산기 테스트 클래스 메서드 분리

--- chapter01/src/next/step/calculator/CalculatorTest.java
+++ chapter01/src/next/step/calculator/CalculatorTest.java
@@ -3,9 +3,24 @@ package next.step.calculator;
 public class CalculatorTest {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
+        add(cal);
+        subtract(cal);
+    }
+
+    private static void add(Calculator cal) {
         System.out.println(cal.add(9, 3));
+
+    }
+
+    private static void subtract(Calculator cal) {
         System.out.println(cal.subtract(9, 3));
+    }
+
+    private static void multiply(Calculator cal) {
         System.out.println(cal.multiply(9, 3));
+    }
+
+    private static void divide(Calculator cal) {
         System.out.println(cal.divide(9, 3));
     }
 }
