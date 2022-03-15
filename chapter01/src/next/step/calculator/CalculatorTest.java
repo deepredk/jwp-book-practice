e304a6e31fdd337c258cab766098047532e302e2
deepredk <deepred.kim@gmail.com>
2022-03-15 21:06:43 +0900

    52p junit 중복 코드 리팩터링

--- chapter01/src/next/step/calculator/CalculatorTest.java
+++ chapter01/src/next/step/calculator/CalculatorTest.java
@@ -2,18 +2,24 @@ package next.step.calculator;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+    private Calculator cal;
+
+    @Before
+    public void setup() {
+        cal = new Calculator();
+    }
+
     @Test
     public void add() {
-        Calculator cal = new Calculator();
         assertEquals(9, cal.add(6, 3));
     }
 
     @Test
     public void subtract() {
-        Calculator cal = new Calculator();
         assertEquals(3, cal.subtract(6, 3));
     }
 }
